572e05af0e614baa6a44911f0ae9a6b7a6b6db52
Ágata <agatagabriele20@gmail.com>
2020-09-04 13:51:51 -0300

    inicializando arquivo

---
+++ pdf.py
@@ -0,0 +1,14 @@
+from tika import parser
+
+raw = parser.from_file("guia_alimentar_populacao_brasileira_2ed.pdf")
+raw = str(raw)
+
+safe_text = raw.encode('utf-8-sig', errors='ignore').decode('ascii', 'ignore')
+
+safe_text = str(raw).replace("\\", "").replace("\n", "\\n")
+#print('--- safe text ---' )
+#print(safe_text)
+print(type(safe_text))
+text_file = open("Output.txt", "w", encoding='utf-8-sig')
+text_file.write(safe_text)
+text_file.close()
