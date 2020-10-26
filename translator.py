42d52e40b85a2abe94defea0248051de478e0a54
require64bits <tankoaraujo@gmail.com>
2020-10-26 20:16:35 -0300

    feat: protótipo da função de tradução

---
+++ translator.py
@@ -0,0 +1,12 @@
+#pip install googletrans
+from googletrans import Translator
+from spellchecker import SpellChecker
+
+def translator(text):
+    translator = Translator()
+    spell = SpellChecker()
+    textSplit = spell.split_words(text)
+    for word in textSplit:
+        wordTranslated = translator.translate(word, dest='pt')
+        text.replace(word, wordTranslated.text)
+    return text
