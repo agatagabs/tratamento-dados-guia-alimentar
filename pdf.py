c7e9f2ab72cec554f5c1273f0f364a59a45813e6
require64bits <tankoaraujo@gmail.com>
2020-11-02 00:10:51 -0300

    fix: consertando o translator (ainda ta meio bugado)

--- pdf.py
+++ pdf.py
@@ -7,7 +7,7 @@ from tirandoOsAcento import tirandoAcento
 from numerosPorExtenso import numerow
 from transformOrdinalNumberInOrdinalText import transformOrdinalNumberInOrdinalText
 from translator import traduzindo
-raw = parser.from_file(filename="guia_alimentar_populacao_brasileira_2ed.pdf", service='text')
+raw = parser.from_file(filename="capitulo1.pdf", service='text')
 raw = str(raw)
 
 safe_text = raw.encode('utf-8-sig', errors='ignore').decode('ascii', 'ignore')
--- translator.py
+++ translator.py
@@ -1,9 +1,10 @@
 #pip install googletrans
+from spellchecker import SpellChecker
 from googletrans import Translator
 def traduzindo(text):
     translator = Translator()
-    textSplit = text.split()
+    textSplit = SpellChecker().split_words(text)
     for word in textSplit:
         wordTranslated = translator.translate(word, dest='pt')
         text = text.replace(word, wordTranslated.text)
-    return text
+    return text + ' \n'
